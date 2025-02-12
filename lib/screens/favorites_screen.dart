5b6028e0377a94fed295e3f7eff92b07e8a36d9d
Jesus Duran <jesusdrnh@hotmail.com>
2025-02-11 21:16:55 -0600

    DeleteFav

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/favorites_provider.dart';
+import '../models/image_model.dart';
 
 class FavoritesScreen extends StatelessWidget {
   @override
@@ -27,9 +28,42 @@ class FavoritesScreen extends StatelessWidget {
                     height: 50,
                     fit: BoxFit.cover,
                   ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _confirmDelete(context, image),
+                  ),
                 );
               },
             ),
+    );
+  }
+
+  void _confirmDelete(BuildContext context, ImageModel image) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete ${image.tittle}?'),
+          content: Text(
+              'Are you sure you want to delete ${image.tittle} from your favorites?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Provider.of<FavoritesProvider>(context, listen: false)
+                    .removeFavorite(image);
+                Navigator.of(context).pop();
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
